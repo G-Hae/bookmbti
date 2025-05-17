571a028b9864a89b30fe3cbbd95c26bf7c83df38
G-Hae <sifialee@gmail.com>
2025-05-17 17:11:44 +0900

    feat: 초기설정 메인 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import styles from './page.module.css';
+import { Button, Container, Text, Title } from '@mantine/core';
 
 export default function Home() {
-    return <div className={styles.page}>나는 새로 시작한다!</div>;
+    return (
+        <Container>
+            <Title order={1} my="md">
+                나는 어떤 책일까?
+            </Title>
+            <Text mb="lg">나를 책으로 표현한다면 나는 어떤 책일까요?</Text>
+            <Button>나는 무슨 책인지 알아보기</Button>
+        </Container>
+    );
 }
